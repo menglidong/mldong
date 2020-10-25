5be4c9268d048655875db45ba8450204aeb517a5
mldong <524719755@qq.com>
2020-10-25 21:15:18 +0800

    json新增单个key解析的方法

--- mldong-framework/mldong-common-base/src/main/java/com/mldong/common/tool/JsonTool.java
+++ mldong-framework/mldong-common-base/src/main/java/com/mldong/common/tool/JsonTool.java
@@ -1,10 +1,13 @@
 package com.mldong.common.tool;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  * json处理工具
@@ -73,4 +76,93 @@ public class JsonTool {
 			return null;
 		} 
 	}
+
+	/**
+	 * 解析当个key-字符串
+	 * @param body
+	 * @param field
+	 * @return
+	 */
+	public static String parseString(String body, String field) {
+		JsonNode node = null;
+		try {
+			node = objectMapper.readTree(body);
+			JsonNode leaf = node.get(field);
+			if(leaf != null)
+				return leaf.asText();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/**
+	 * 解析当个key-整型
+	 * @param body
+	 * @param field
+	 * @return
+	 */
+	public static Integer parseInteger(String body, String field) {
+		JsonNode node = null;
+		try {
+			node = objectMapper.readTree(body);
+			JsonNode leaf = node.get(field);
+			if(leaf != null)
+				return leaf.asInt();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/**
+	 * 解析当个key-列表
+	 * @param body
+	 * @param field
+	 * @return
+	 */
+	public static List<Integer> parseIntegerList(String body, String field) {
+		JsonNode node = null;
+		try {
+			node = objectMapper.readTree(body);
+			JsonNode leaf = node.get(field);
+
+			if(leaf != null)
+				return objectMapper.convertValue(leaf, new TypeReference<List<Integer>>(){});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+
+	public static Boolean parseBoolean(String body, String field) {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = null;
+		try {
+			node = mapper.readTree(body);
+			JsonNode leaf = node.get(field);
+			if(leaf != null)
+				return leaf.asBoolean();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Short parseShort(String body, String field) {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = null;
+		try {
+			node = mapper.readTree(body);
+			JsonNode leaf = node.get(field);
+			if(leaf != null) {
+				Integer value = leaf.asInt();
+				return value.shortValue();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
