dde1ed8eed5164a3d54f26ea5bdba6e13b805236
mldong <524719755@qq.com>
2021-01-05 09:58:12 +0800

    查询文章单条记录新增ext_article_form_config字段

--- mldong-cms-parent/mldong-cms-domain/src/main/java/com/mldong/modules/cms/dto/CmsArticleWithExt.java
+++ mldong-cms-parent/mldong-cms-domain/src/main/java/com/mldong/modules/cms/dto/CmsArticleWithExt.java
@@ -8,6 +8,8 @@ public class CmsArticleWithExt extends CmsArticle {
     private String categoryName;
     @ApiModelProperty(value = "栏目图标")
     private String categoryIcon;
+    @ApiModelProperty(value = "文章扩展信息配置")
+    private String extArticleFormConfig;
     public String getCategoryName() {
         return categoryName;
     }
@@ -22,5 +24,13 @@ public class CmsArticleWithExt extends CmsArticle {
 
     public void setCategoryIcon(String categoryIcon) {
         this.categoryIcon = categoryIcon;
+    }
+
+    public String getExtArticleFormConfig() {
+        return extArticleFormConfig;
+    }
+
+    public void setExtArticleFormConfig(String extArticleFormConfig) {
+        this.extArticleFormConfig = extArticleFormConfig;
     }
 }
