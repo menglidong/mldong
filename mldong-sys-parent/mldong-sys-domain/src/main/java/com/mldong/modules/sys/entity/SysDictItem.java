49a98676c0cb3c41d54cd0375f7e75abdf628450
mldong <524719755@qq.com>
2021-01-20 08:46:35 +0800

    去掉字典与字典项的的逻辑删除

--- mldong-sys-parent/mldong-sys-domain/src/main/java/com/mldong/modules/sys/entity/SysDictItem.java
+++ mldong-sys-parent/mldong-sys-domain/src/main/java/com/mldong/modules/sys/entity/SysDictItem.java
@@ -7,7 +7,6 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import tk.mybatis.mapper.annotation.LogicDelete;
 import com.mldong.common.base.YesNoEnum;
 
 /**
@@ -40,7 +39,6 @@ public class SysDictItem implements Serializable{
     @ApiModelProperty(value = "更新时间")
     private Date updateTime;
     @ApiModelProperty(value = "是否删除(1->未删除|NO,2->已删除|YES)")
-	@LogicDelete(isDeletedValue=YesNoEnum.Y,notDeletedValue=YesNoEnum.N)
     private YesNoEnum isDeleted;
 
     /**
