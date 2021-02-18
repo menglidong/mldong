d06a84014736b18f68b517668b3e78a4a729ec29
mldong <524719755@qq.com>
2021-02-18 20:14:36 +0800

    开发与测试环境，验证码默认为8888

--- mldong-framework/mldong-sms-service/src/main/java/com/mldong/controller/SmsController.java
+++ mldong-framework/mldong-sms-service/src/main/java/com/mldong/controller/SmsController.java
@@ -9,6 +9,7 @@ import com.mldong.common.tool.StringTool;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,17 +25,24 @@ public class SmsController {
     private RedisTemplate<String,String> redisTemplate;
     @Autowired(required = false)
     private SmsHandler smsHandler;
+    @Value("${spring.profiles.active}")
+    private String profiles;
     private static final String SMS_KEY = "SMS_CODE:";
     @PostMapping("sendSms")
     @ApiOperation(value = "发送短信验证码")
     @AuthIgnore
     public CommonResult<?> sendSms(@RequestBody @Validated PhoneParam param) {
         String uuid = Md5Tool.md5(param.getMobilePhone());
-        String smsCode = StringTool.getRandomNumber(4);
-        redisTemplate.opsForValue().set(SMS_KEY+uuid, smsCode, 5, TimeUnit.MINUTES);
+        String smsCode = "8888";
+        if(!("dev".equals(profiles) || "test".equals(profiles))) {
+            // 非开发与测试
+            smsCode = StringTool.getRandomNumber(4);
+        }
+        // 是否发送逻辑还是由处理类来定吧，就不放在上面条件中限制了
         if(smsHandler !=null) {
             smsHandler.sendOut(param.getMobilePhone(),smsCode);
         }
+        redisTemplate.opsForValue().set(SMS_KEY+uuid, smsCode, 5, TimeUnit.MINUTES);
         return CommonResult.success();
     }
 }
