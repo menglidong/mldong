fe0f24d9885dceb84819d8d5a60f0e1598bcb086
mldong <mldong666@qq.com>
2023-12-14 15:36:26 +0800

    增加演示环境权限拦截处理

--- mldong-framework/mldong-base/src/main/java/com/mldong/auth/AuthInterceptor.java
+++ mldong-framework/mldong-base/src/main/java/com/mldong/auth/AuthInterceptor.java
@@ -2,10 +2,19 @@ package com.mldong.auth;
 
 import cn.dev33.satoken.fun.SaParamFunction;
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import com.mldong.auth.err.AuthErrEnum;
+import com.mldong.exception.ServiceException;
 import com.mldong.web.LoginUserHolder;
+import org.springframework.core.env.Environment;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 自定义拦截器
@@ -16,9 +25,19 @@ public class AuthInterceptor extends SaInterceptor {
     public AuthInterceptor(SaParamFunction<Object> auth) {
         super(auth);
     }
-
+    private static final Environment environment = SpringUtil.getBean(Environment.class);
+    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    private static final String profileActive = environment.getProperty("spring.profiles.active",String.class);
+    private static final List<String> blacklist = environment.getProperty("blacklist",List.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(ObjectUtil.equals(profileActive,"demo")) {
+            // 演示环境，无权限操作
+            String uri = request.getRequestURI();
+            if(CollectionUtil.isNotEmpty(blacklist) && blacklist.stream().anyMatch(s -> antPathMatcher.match(s,uri))) {
+                ServiceException.throwBiz(AuthErrEnum.NO_RESOURCE_AUTH);
+            }
+        }
         LoginUser loginUser = LoginUserHolder.me();
         if(loginUser!=null && loginUser.isSuperAdmin()) {
             // 超级管理员直接放行
