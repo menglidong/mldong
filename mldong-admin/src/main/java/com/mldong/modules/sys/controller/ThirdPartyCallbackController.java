c7395f66775633b01c52e15750c7a3d42676f793
mldong <mldong666@qq.com>
2023-12-15 12:24:49 +0800

    校验是否点亮项目

--- mldong-admin/src/main/java/com/mldong/modules/sys/controller/ThirdPartyCallbackController.java
+++ mldong-admin/src/main/java/com/mldong/modules/sys/controller/ThirdPartyCallbackController.java
@@ -2,6 +2,8 @@ package com.mldong.modules.sys.controller;
 
 import cn.dev33.satoken.annotation.SaIgnore;
 import cn.hutool.core.lang.Dict;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.mldong.base.CommonResult;
@@ -11,6 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,21 +29,26 @@ public class ThirdPartyCallbackController {
     @PostMapping("/thirdParty/checkStarred")
     @ApiOperation(value="校验是否点亮项目")
     @SaIgnore
-    public CommonResult<?> checkStarred(String code) {
-        String url = "https://gitee.com/oauth/token?grant_type=authorization_code&code="+code;
+    public CommonResult<?> checkStarred(@RequestBody Dict args) {
+        String code = args.getStr("code");
         Dict params = Dict.create();
+        params.put("code", code);
         params.put("client_id", ConstantContextHolder.getSysConfig("GITEE_CLIENT_ID",String.class,false));
         params.put("client_secret",ConstantContextHolder.getSysConfig("GITEE_CLIENT_SECRET",String.class,false));
-        String body = HttpUtil.post(url, params);
+        params.put("redirect_uri","https://flow.mldong.com/oauth/gitee");
+        String url = StrUtil.format("https://gitee.com/oauth/token?grant_type=authorization_code&code={code}&client_id={client_id}&redirect_uri={redirect_uri}&client_secret={client_secret}",params);
+        log.info("url:{}",url);
+        String body = HttpUtil.post(url,"");
         log.info("get_gitee_token:{}",body);
         Dict bodyObj = JSONUtil.toBean(body, Dict.class);
         String accessToken = bodyObj.getStr("access_token");
         log.info("access_token:{}",accessToken);
-        String owner = "https://gitee.com/mldong";
-        String repo = "https://gitee.com/mldong/mldong";
-        String checkUrl = "https://gitee.com/api/v5/user/starred";
-        body = HttpUtil.get(checkUrl, Dict.create().set("owner",owner).set("repo",repo).set("access_token", accessToken));
+        String owner = "mldong";
+        String repo = "mldong";
+        String checkUrl = StrUtil.format("https://gitee.com/api/v5/user/starred/{}/{}?access_token={}",owner,repo,accessToken);
+        log.info("checkUrl:{}",checkUrl);
+        body = HttpUtil.get(checkUrl);
         log.info("get_gitee_starred:{}",body);
-        return CommonResult.ok();
+        return CommonResult.data(Dict.of("flag", ObjectUtil.isEmpty(body)));
     }
 }
