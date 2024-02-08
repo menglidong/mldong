b3a66fda4503b94103b4a7398720ad89afa4f3d0
mldong <mldong666@qq.com>
2024-02-08 14:38:08 +0800

    新增GlobalProperties,调整blackList配置方式

---
+++ mldong-framework/mldong-base/src/main/java/com/mldong/properties/GlobalProperties.java
@@ -0,0 +1,27 @@
+package com.mldong.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 全局属性
+ * @author mldong
+ * @date 2024/2/8
+ */
+@Configuration
+@ConfigurationProperties(prefix = "g")
+@Data
+public class GlobalProperties {
+    /**
+     * 演示环境黑名单，该名单下的url不允许访问
+     */
+    private List<String> blacklist =  new ArrayList<>();
+    /**
+     * 忽略权限的url
+     */
+    private List<String> ignoreUrlList = new ArrayList<>();
+}
